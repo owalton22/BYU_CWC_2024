572d9b16cc36dd06a8a6147872612d547f63dcd4
Olivia Walton <walton.oliviarose@gmail.com>
2023-10-17 10:05:49 -0600

    Brake testing basic software added

---
+++ Concept_Exploration/Brakes/brake_control/brake_control.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+#define BRAKE_PIN 3
+#define BRAKE_DISENGAGED 30
+#define BRAKE_ENGAGED 10
+
+Servo brakeControl;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(BRAKE_PIN, OUTPUT);
+
+  brakeControl.write(BRAKE_DISENGAGED);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(3000);
+
+  brakeControl.write(BRAKE_ENGAGED);
+
+  delay(3000);
+
+  brakeControl.write(BRAKE_DISENGAGED);
+}
