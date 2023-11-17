8012165c2ead11e1dedc6b85a3806bd26debaff9
katienan <108026662+katienan@users.noreply.github.com>
2023-11-17 09:38:47 -0700

    Update linear_actuator.ino
    Tests continuous extension to a half extension (from 1ms to 1.5ms) for a servo with 2ms retract pulse and 1ms extend pulse.
    
    Commented code goes from full extension to full retraction, out to half extension, retracts, and repeats that sequence. Comment out while loop and iterating "half" global to implement. Replace "half" with "HALF" as well.

--- Concept_Exploration/Linear_Actuator/linear_actuator/linear_actuator.ino
+++ Concept_Exploration/Linear_Actuator/linear_actuator/linear_actuator.ino
@@ -1,14 +1,43 @@
-#define PWM_PIN 3
+#include <Servo.h>
+#define LIN_ACTUATOR_PIN 3
+#define EXTEND 1000 // Pulse width in microseconds corresponding to full extension
+#define RETRACT 2000 // Pulse width in microseconds corresponding to full retraction
+#define HALF 1500 // Pulse width in microseconds corresponding half extension
+
+int half = 1000; // Increment this to test granularity
+Servo linActuator;
+int position;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PWM_PIN, OUTPUT);
+  Serial.begin(9600);
+  linActuator.attach(LIN_ACTUATOR_PIN);
+  linActuator.writeMicroseconds(RETRACT); // Write a value between 1000 and 2000 with this function to adjust position
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i = 25; i < 50; i++) {
-    analogWrite(PWM_PIN, i);
-    delay(100);
+  // linActuator.writeMicroseconds(EXTEND_L12);
+  // Serial.println("Linear actuator set to full extension: ");
+  // position = linActuator.read();
+  // Serial.println(position);
+  // delay(3000); // delay for 3 seconds
+  // linActuator.writeMicroseconds(RETRACT_L12);
+  // Serial.println("Linear actuator set to full retraction: ");
+  // position = linActuator.read();
+  // Serial.println(position);
+  // delay(3000); // delay for 3 seconds
+  linActuator.writeMicroseconds(half);
+  Serial.println("Linear actuator set to half extension: ");
+  float position = linActuator.read();
+  Serial.println(position);
+  //delay(500); // delay for 3 seconds
+  // linActuator.writeMicroseconds(RETRACT_L12);
+  // Serial.println("Linear actuator set to full retraction: ");
+  // position = linActuator.read();
+  // Serial.println(position);
+  // delay(3000); // delay for 3 seconds
+  half = half + 1; // Comment out when just doing range test.
+  if (half > 1500){
+    while(1); 
   }
 }
